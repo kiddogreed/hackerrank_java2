d7d11c60a38332ad72d39c5b900695f96c397e92
john russelle domingo <johnrusselledomingo@gmail.com>
2023-04-14 10:47:19 +0800

    update

--- LexicographOrder.java
+++ LexicographOrder.java
@@ -9,13 +9,19 @@ public class LexicographOrder {
 
   public static void main(String[] args) {
     
-    Scanner sc = new Scanner(System.in);  
+    // Scanner sc = new Scanner(System.in);  
     
-    System.out.println("add lexicograph words:");
-    String words = sc.nextLine();
-    int numberLimit = sc.nextInt();
-    System.out.println("add lexicograph limit");
-    System.out.println(getSmallestAndLargest(words, numberLimit));
+    // System.out.println("add lexicograph words:");
+    // String words = sc.nextLine();
+    // int numberLimit = sc.nextInt();
+    // System.out.println("add lexicograph limit");
+    // System.out.println(getSmallestAndLargest(words, numberLimit));
+
+
+    String ss = "welcometojava";
+    int kk = 3;
+
+    System.out.println(ss.substring(0,kk));
     
 
 
@@ -24,22 +30,18 @@ public class LexicographOrder {
   }
 
   public static String  getSmallestAndLargest(String s, int k) {
-
     String smallest = "";
     String largest = "";
+    
+    smallest = s.substring(0,k);
     largest = s.substring(0,k);
-    smallest = s.substring(0, k);
-
-
-    for (int i = 0; i < s.length()-k; i++) {
-
-      if(largest.compareTo(s.substring(i,k+i))<0)
-      largest=s.substring(i,k+i);
-      
-      if(smallest.compareTo(s.substring(i,k+i))>0)
-      smallest=s.substring(i,k+i);
-
-    }
+    
+for(int i=1;i<s.length()-k+1;i++){
+    if(smallest.compareTo(s.substring(i,i+k)) > 0 )
+        smallest=s.substring(i,i+k);
+    if(largest.compareTo(s.substring(i,i+k)) < 0 )     
+        largest=s.substring(i,i+k);
+}
 
 
     
